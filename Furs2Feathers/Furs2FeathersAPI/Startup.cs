6d8b01a5311f55c265f759b56142258769a82726
Haroldoa <haroldo.j.altamirano@biola.edu>
2020-04-28 19:54:13 -0500

    added nick stuff

--- Furs2Feathers/Furs2FeathersAPI/Startup.cs
+++ Furs2Feathers/Furs2FeathersAPI/Startup.cs
@@ -5,14 +5,18 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 namespace Furs2FeathersAPI
 {
     public class Startup
     {
+        private const string CorsPolicyName = "AllowConfiguredOrigins";
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -23,7 +27,63 @@ namespace Furs2FeathersAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
+            services.AddApplicationInsightsTelemetry();
+
+            // switch between database providers using runtime configuration
+            // (the existing migrations are SQL-Server-specific, but the model itself is not)
+
+            // this should be the name of a connection string.
+           /* string whichDb = Configuration["DatabaseConnection"];
+            if (whichDb is null)
+            {
+                throw new InvalidOperationException($"No value found for \"DatabaseConnection\"; unable to connect to a database.");
+            }
+
+            string connection = Configuration.GetConnectionString(whichDb);
+            if (connection is null)
+            {
+                throw new InvalidOperationException($"No value found for \"{whichDb}\" connection; unable to connect to a database.");
+            }*/
+
+            /*if (whichDb.Contains("PostgreSql", StringComparison.InvariantCultureIgnoreCase))
+            {
+                services.AddDbContext<NotesContext>(options =>
+                    options.UseNpgsql(connection));
+            }
+            else
+            {
+                services.AddDbContext<NotesContext>(options =>
+                    options.UseSqlServer(connection));
+            }*/
+
+           /* services.AddScoped<INoteRepository, NoteRepository>();*/
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Notes API", Version = "v1" });
+            });
+
+            // support switching between database providers using runtime configuration
+
+            var allowedOrigins = Configuration.GetSection("CorsOrigins").Get<string[]>();
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy(CorsPolicyName, builder =>
+                    builder.WithOrigins(allowedOrigins ?? Array.Empty<string>())
+                        .AllowAnyMethod()
+                        .AllowAnyHeader()
+                        .AllowCredentials());
+            });
+
+            services.AddControllers(options =>
+            {
+                // remove the default text/plain string formatter to clean up the OpenAPI document
+                options.OutputFormatters.RemoveType<StringOutputFormatter>();
+
+                options.ReturnHttpNotAcceptable = true;
+                options.SuppressAsyncSuffixInActionNames = false;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -33,23 +93,28 @@ namespace Furs2FeathersAPI
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
 
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
+            if (Configuration.GetValue("UseHttpsRedirection", defaultValue: true) is true)
+            {
+                app.UseHttpsRedirection();
+            }
 
             app.UseRouting();
 
+            app.UseCors(CorsPolicyName);
+
             app.UseAuthorization();
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Notes API V1");
+            });
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapRazorPages();
+                endpoints.MapControllers();
             });
         }
     }
