05a7d095bebed2d4e6df769c91113731b3a7a9da
Haroldoa <haroldo.j.altamirano@biola.edu>
2020-05-05 17:40:48 -0500

    Customers controller decoupled from entity framework

---
+++ Furs2Feathers/Furs2Feathers.Domain/Interfaces/ICustomerRepository.cs
@@ -0,0 +1,26 @@
+﻿using Furs2Feathers.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Furs2Feathers.Domain.Interfaces
+{
+    public interface ICustomerRepository
+    {
+        void Add(Customer entity);
+        void AddRange(IEnumerable<Customer> entities);
+        bool Any(Expression<Func<Customer, bool>> predicate);
+        IEnumerable<Customer> FindAsync(Expression<Func<Customer, bool>> predicate);
+        Task<Customer> FindAsync(int id);
+        Task<Customer> FindAsyncAsNoTracking(int id);
+        Task<IEnumerable<Customer>> GetAll();
+        Task<Customer> GetAsync(int id);
+        Task<bool> ModifyStateAsync(Customer address, int id);
+        void Remove(Customer entity);
+        void RemoveRange(IEnumerable<Customer> entities);
+        void Save();
+        Task<int> SaveChangesAsync();
+        Task<IEnumerable<Customer>> ToListAsync();
+    }
+}
