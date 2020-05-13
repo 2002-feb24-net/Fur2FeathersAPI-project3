892cd00a3c7439e2f9912155447156f454b5ea5e
abraham <Abrahambergerson@gmail.com>
2020-05-12 22:35:55 -0500

    customer testing not complete

--- Furs2Feathers/Furs2Feathers.Test/Controllers/CustomersControllerTests.cs
+++ Furs2Feathers/Furs2Feathers.Test/Controllers/CustomersControllerTests.cs
@@ -1,14 +1,37 @@
-﻿using System;
+﻿using FluentAssertions;
+using Furs2Feathers.Domain.Interfaces;
+using Furs2Feathers.Domain.Models;
+using Furs2FeathersAPI.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace Furs2Feathers.Test.Controllers
 {
     public class CustomersControllerTests
     {
-        [Fact]
-        public void Test1()
-        {
 
+        public CustomersControllerTests()
+        {
+            customerRepo = new Mock<ICustomerRepository>();
+        }
+
+        private Mock<ICustomerRepository> customerRepo { get; set; }
+        private readonly ILogger<CustomersController> logger;
+
+        [Fact]
+        public async void GetCustomer()
+        {
+            var listOfCustomers = new List<Customer>();
+
+            var customerController = new CustomersController(customerRepo.Object, logger);
+            var result = await customerController.GetCustomers();
+            var valuesOfresult = (result.Result as OkObjectResult).Value;
+            valuesOfresult.Should().BeEquivalentTo(listOfCustomers);
+            customerController.Should().NotBeNull();
         }
     }
 }
